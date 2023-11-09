ff79067357cfc7fd9d950eaa80ba9181df6f81c8
niikofilms <n.ort@outlook.com>
2023-11-08 21:16:01 -0500

    AvancesV1

--- Concurso_UNAL/Assets/SCRIPTS/Input/PlayerController.cs
+++ Concurso_UNAL/Assets/SCRIPTS/Input/PlayerController.cs
@@ -4,6 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody _rigidbody = null;
+    [SerializeField] PlayerControllerInput _input;
+    
+    Vector3 _playerMoveInput = Vector3.zero;
+
+    [Header("Movement")]
+    [SerializeField] float _movementMultiplier = 30.0f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _playerMoveInput = GetMoveInput();
+        PlayerMove();
+
+        _rigidbody.AddRelativeForce(_playerMoveInput, ForceMode.Force);
+    }
+
+    private Vector3 GetMoveInput()
+    {
+        return new Vector3(_input.MoveInput.x, 0.0f, _input.MoveInput.y);
+    }
+
+    private void PlayerMove()
+    {
+        _playerMoveInput = (new Vector3(_playerMoveInput.x * _movementMultiplier * _rigidbody.mass,
+                                        _playerMoveInput.y,
+                                        _playerMoveInput.z * _movementMultiplier * _rigidbody.mass));
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Concurso_UNAL/Assets/SCRIPTS/Input/PlayerControllerInput.cs
+++ Concurso_UNAL/Assets/SCRIPTS/Input/PlayerControllerInput.cs
@@ -9,26 +9,26 @@ public class PlayerControllerInput : MonoBehaviour
 
     public Vector2 LookInput { get; private set; } = Vector2.zero;
 
-    InputAction _input = null;
+    InputSystem _input = null;
 
     private void OnEnable()
     {
-        _input = new InputAction();
+        _input = new InputSystem();
         _input.PlayerController.Enable();
 
-        _input.PlayerController.Move.perfomed += SetMove;
+        _input.PlayerController.Move.performed += SetMove;
         _input.PlayerController.Move.canceled += SetMove;
 
-        _input.PlayerController.Look.perfomed += SetLook;
+        _input.PlayerController.Look.performed += SetLook;
         _input.PlayerController.Look.canceled += SetLook;
     }
 
     private void OnDisable()
     {
-        _input.PlayerController.Move.perfomed -= SetMove;
+        _input.PlayerController.Move.performed -= SetMove;
         _input.PlayerController.Move.canceled -= SetMove;
 
-        _input.PlayerController.Look.perfomed -= SetLook;
+        _input.PlayerController.Look.performed -= SetLook;
         _input.PlayerController.Look.canceled -= SetLook;
 
         _input.PlayerController.Disable();
